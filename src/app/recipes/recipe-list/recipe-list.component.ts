1fd61101e82f1951245321e81aed54d524b75877
romiki44 <romiki44@gmail.com>
2019-11-02 15:08:05 +0100

    section3 done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] =[
+    new Recipe('A test recipe', 'Recipe for testing', 'https://iowagirleats-iowagirleats.netdna-ssl.com/wp-content/uploads/2015/10/Crock-Pot-Chicken-and-Wild-Rice-Soup-iowagirleats-04.jpg'),
+    new Recipe('A test recipe', 'Recipe for testing', 'https://culinaryarts.ca/wp-content/uploads/2015/08/Recipes-Widget.jpg')
+  ];
 
   constructor() { }
 
