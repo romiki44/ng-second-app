88db71c424d0972f0950df4b6b409105a90be34b
romiki44 <romiki44@gmail.com>
2019-11-03 17:47:28 +0100

    section6 done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
   recipes:Recipe[] =[
-    new Recipe('A test recipe', 'Recipe for testing', 'https://iowagirleats-iowagirleats.netdna-ssl.com/wp-content/uploads/2015/10/Crock-Pot-Chicken-and-Wild-Rice-Soup-iowagirleats-04.jpg'),
-    new Recipe('A test recipe', 'Recipe for testing', 'https://culinaryarts.ca/wp-content/uploads/2015/08/Recipes-Widget.jpg')
+    new Recipe('First recipe', 'Meat recipe', 'https://iowagirleats-iowagirleats.netdna-ssl.com/wp-content/uploads/2015/10/Crock-Pot-Chicken-and-Wild-Rice-Soup-iowagirleats-04.jpg'),
+    new Recipe('Second recipe', 'Vegatable recipe', 'https://culinaryarts.ca/wp-content/uploads/2015/08/Recipes-Widget.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
